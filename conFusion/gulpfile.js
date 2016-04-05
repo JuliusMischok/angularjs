ade2018376a83c453edd71c3b19fcc0b75212970
JuliusMischok <julius.mischok@gmail.com>
2016-04-05 17:48:47 +0200

    Week 3 - Exercise 2

--- conFusion/gulpfile.js
+++ conFusion/gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-	return gulp.src('./app/menu.html')
+	return gulp.src('./app/*.html')
 	.pipe(usemin({
 		css:[minifycss(),rev()],
 		js: [ngannotate(),uglify(),rev()]
@@ -75,8 +75,7 @@ gulp.task('browser-sync', ['default'], function () {
 
 	browserSync.init(files, {
 		server: {
-			baseDir: "dist",
-			index: "dishdetail.html"
+			baseDir: "dist"
 		}
 	});
 	// Watch any files in dist/, reload on change
